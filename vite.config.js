48aec5f92a189ff34bc281298f6097fcc3fb6962
Julio Oliveira <julio.rui.oliveira@gmail.com>
2026-04-16 23:46:35 -0300

    Corrige configuração do Vite

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+    react(),
+    tailwindcss(),
+    ],
+    resolve: {
+    alias: {
+      // Alias @ to the src directory
+      '@': path.resolve(__dirname, './src'),
+    },
+    },
     base: '/beach-tennis-pro/',
-    server: {
-        port: 3050,
-    }
+    // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+    assetsInclude: ['**/*.svg', '**/*.csv'],
 });
